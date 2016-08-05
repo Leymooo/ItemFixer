028f69d4f2e5bbf64f7be0b1ff2b87c894907eea
Dimatert9 <mikimouse100@mail.ru>
2016-08-05 16:06:44 +0300

    2.5.3

--- src/ru/xtime/nbtfix/Main.java
+++ src/ru/xtime/nbtfix/Main.java
@@ -17,12 +17,16 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.wrappers.nbt.NbtBase;
 import com.comphenix.protocol.wrappers.nbt.NbtCompound;
 import com.comphenix.protocol.wrappers.nbt.NbtFactory;
+import com.comphenix.protocol.wrappers.nbt.NbtList;
 
 public class Main extends JavaPlugin implements Runnable {
     Boolean hasUpdates;
     Boolean mc17;
+    Boolean skullfix;
+    Boolean removeInvalidEnch;
     private ArrayList<String> nbt = new ArrayList<String>();
     private ArrayList<String> eggs = new ArrayList<String>();
     private ArrayList<String> armor = new ArrayList<String>();
@@ -30,46 +34,124 @@ public class Main extends JavaPlugin implements Runnable {
     private ArrayList<String> inventory = new ArrayList<String>();
     public void onEnable() {
         hasUpdates = false;
-        mc17=this.getServer().getClass().getPackage().getName().replace(".",  ",").split(",")[3].startsWith("v1_7_R");
-        this.saveDefaultConfig();
-        nbt.addAll(this.getConfig().getStringList("nbt"));
-        eggs.addAll(this.getConfig().getStringList("spawneggs"));
-        armor.addAll(this.getConfig().getStringList("armorstand"));
-        book.addAll(this.getConfig().getStringList("writenbook"));
-        inventory.addAll(this.getConfig().getStringList("inventory"));
+        mc17 = this.getServer().getClass().getPackage().getName().replace(".",  ",").split(",")[3].startsWith("v1_7_R");
+        config();
         ProtocolLibrary.getProtocolManager().addPacketListener(new NBTHeldItemListener(this));
         ProtocolLibrary.getProtocolManager().addPacketListener(new NBTCreatListener(this));
         this.getServer().getScheduler().runTaskTimerAsynchronously(this, this, 0, 18000L);
         this.msgToCS("&aItemFixer включен");
     }
-    private void removeEnt(final ItemStack item) {
+    private void config() {
+        saveDefaultConfig();
+        if (!getConfig().isSet("remove-invalid-enchants")) getConfig().set("remove-invalid-enchants", false);
+        if (!getConfig().isSet("fix-skull-exploit")) getConfig().set("fix-skull-exploit", true);
+        if (!getConfig().isSet("nbt")) {
+            ArrayList<String> list = new ArrayList<String>();
+            list.add("Items");
+            list.add("ActiveEffects");
+            list.add("Command");
+            list.add("CustomName");
+            list.add("AttributeModifiers");
+            list.add("Unbreakable");
+            list.add("CustomPotionEffects");
+            getConfig().set("nbt", list);
+        }
+        if (!getConfig().isSet("spawneggs")) {
+            ArrayList<String> list = new ArrayList<String>();
+            list.add("Fuse");
+            list.add("CustomNames");
+            list.add("powered");
+            list.add("ActiveEffects");
+            list.add("ExplosionPower");
+            getConfig().set("spawneggs", list);
+        }
+        if (!getConfig().isSet("armorstand")) {
+            ArrayList<String> list = new ArrayList<String>();
+            list.add("EntityTag");
+            getConfig().set("armorstand", list);
+        }
+        if (!getConfig().isSet("writtenbook")) {
+            ArrayList<String> list = new ArrayList<String>();
+            list.add("ClickEvent");
+            list.add("run_command");
+            getConfig().set("writtenbook", list);
+        }
+        if (!getConfig().isSet("inventory")) {
+            ArrayList<String> list = new ArrayList<String>();
+            list.add("BlockEntityTag");
+            getConfig().set("inventory", list);
+        }
+        saveConfig();
+        saveDefaultConfig();
+        reloadConfig();
+        removeInvalidEnch = this.getConfig().getBoolean("remove-invalid-enchants");
+        skullfix = this.getConfig().getBoolean("fix-skull-exploit");
+        nbt.addAll(this.getConfig().getStringList("nbt"));
+        eggs.addAll(this.getConfig().getStringList("spawneggs"));
+        armor.addAll(this.getConfig().getStringList("armorstand"));
+        book.addAll(this.getConfig().getStringList("writtenbook"));
+        inventory.addAll(this.getConfig().getStringList("inventory"));
+    }
+    private void removeEnt(ItemStack item) {
         if (item == null) return;
         if (item.getType() == Material.AIR) return;
         if (item.getEnchantments() == null) return;
         for (final Map.Entry<Enchantment, Integer> ench : item.getEnchantments().entrySet()) {
+            final ItemMeta meta = item.getItemMeta();
             final Enchantment Enchant = ench.getKey();
-            if (ench.getValue() > Enchant.getMaxLevel() || ench.getValue() < 0) {
-                final ItemMeta meta = item.getItemMeta();
-                meta.removeEnchant(Enchant);
-                item.setItemMeta(meta);
-            }
+            if (!Enchant.canEnchantItem(item) && removeInvalidEnch) meta.removeEnchant(Enchant);
+            if (ench.getValue() > Enchant.getMaxLevel() || ench.getValue() < 0) meta.removeEnchant(Enchant);
+            item.setItemMeta(meta);
         }
         return;
     }
-
+    @SuppressWarnings("rawtypes")
+    private boolean isExploitSkull(NbtCompound tag) {
+        if (tag.containsKey("SkullOwner")) {
+            NbtCompound skullOwner = tag.getCompound("SkullOwner");
+            if (skullOwner.containsKey("Properties")) {
+                NbtCompound properties = skullOwner.getCompound("Properties");
+                if (properties.containsKey("textures")) {
+                    NbtList<NbtBase> textures = properties.getList("textures");
+                    for (NbtBase texture : textures.asCollection()) {
+                        if (texture instanceof NbtCompound) {
+                            if (!((NbtCompound) texture).containsKey("Signature")) {
+                                if (((NbtCompound) texture).containsKey("Value")) {
+                                    if (((NbtCompound) texture).getString("Value").trim().length() > 0) {
+                                        return false;
+                                    }
+                                }
+                                tag.remove("SkullOwner");
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
     public boolean isExploit(ItemStack stack, Player p) {
         boolean b = false;
         try {
             removeEnt(stack);
             Material mat = stack.getType();
             NbtCompound tag = (NbtCompound) NbtFactory.fromItemTag(stack);
+            // Фиксим CrashChest. CrashItem // Фиксим возможную утечку.
+            if (mat == Material.CHEST || mat == Material.NAME_TAG) {
+                if (tag.toString().length() > 1000) {
+                    tag.getKeys().clear();
+                    b = true;
+                }
+            }
+            //
             for (String a : nbt) {
                 if (tag.containsKey(a)) {
                     tag.remove(a);
                     b = true;
                 }
             }
-            if (mat == Material.CHEST || mat == Material.TRAPPED_CHEST || mat == Material.DROPPER || mat == Material.DISPENSER) {
+            if (mat == Material.CHEST || mat == Material.TRAPPED_CHEST || mat == Material.DROPPER || mat == Material.DISPENSER || mat == Material.COMMAND || mat == Material.COMMAND_MINECART) {
                 for (String a : inventory) {
                     if (tag.containsKey(a)) {
                         tag.remove(a);
@@ -86,7 +168,7 @@ public class Main extends JavaPlugin implements Runnable {
             } else if (mat == Material.MONSTER_EGG || mat == Material.MONSTER_EGGS) {
                 for (String a : eggs) {
                     if (tag.toString().contains(a)) {
-                        p.getInventory().remove(stack);
+                        tag.getKeys().clear();
                         b = true;
                     }
                 }
@@ -97,6 +179,8 @@ public class Main extends JavaPlugin implements Runnable {
                         b = true;
                     }
                 }
+            } else if ((mat == Material.SKULL || mat == Material.SKULL_ITEM) && skullfix && stack.getDurability() == 3) {
+                if (isExploitSkull(tag)) b = true;
             }
         } catch (Exception e) {
             return b;
--- src/ru/xtime/nbtfix/NBTCreatListener.java
+++ src/ru/xtime/nbtfix/NBTCreatListener.java
@@ -1,6 +1,7 @@
 package ru.xtime.nbtfix;
 
 import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
 
@@ -9,13 +10,14 @@ import org.bukkit.inventory.ItemStack;
 
 public class NBTCreatListener extends PacketAdapter {
     public NBTCreatListener(Main plugin) {
-        super(plugin, PacketType.Play.Client.SET_CREATIVE_SLOT);
+        super(plugin, ListenerPriority.HIGHEST, PacketType.Play.Client.SET_CREATIVE_SLOT);
     }
 
     @Override
     public void onPacketReceiving(PacketEvent event) {
-        if (event.getPlayer().hasPermission("itemfixer.bypass")) return;
+        if (event.isCancelled()) return;
         if (event.getPlayer().getGameMode() != GameMode.CREATIVE) return;
+        if (event.getPlayer().hasPermission("itemfixer.bypass")) return;
         ItemStack stack = event.getPacket().getItemModifier().read(0);
         if (((Main) getPlugin()).isExploit(stack, event.getPlayer())){
             event.getPlayer().sendMessage("§cЧитерские вещи запрещены! Если вы продолжите, вы будете забанены!");
