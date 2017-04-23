49ed9b0c945d15d7e951b8fac8f815548701cc4b
CatCoderr <catcoderr@gmail.com>
2017-04-23 19:16:06 +0300

    Добавлен апдейтер

---
+++ src/ru/xtime/nbtfix/updater/UpdaterException.java
@@ -0,0 +1,30 @@
+package ru.xtime.nbtfix.updater;
+
+import java.util.logging.Level;
+
+/**
+ * Created by Ruslan on 23.04.2017.
+ */
+public class UpdaterException extends Exception {
+
+    private final String message;
+    private final PluginUpdater updater;
+
+    public UpdaterException(String message, PluginUpdater updater) {
+        super(message);
+        this.message = message;
+        this.updater = updater;
+    }
+
+    public void print() {
+        updater.getPlugin().getLogger().log(Level.SEVERE, String.format(
+                printableMessage(),
+                updater.getPlugin().getName(),
+                message));
+    }
+
+
+    public String printableMessage() {
+        return "[PluginUpdater]: Error of plugin %s (%s)";
+    }
+}
---
+++ src/ru/xtime/nbtfix/updater/UpdaterResult.java
@@ -0,0 +1,14 @@
+package ru.xtime.nbtfix.updater;
+
+/**
+ * Created by Ruslan on 23.04.2017.
+ */
+public enum UpdaterResult {
+
+    UPDATE_FOUND,
+    UPDATE_NOT_FOUND;
+
+    public boolean hasUpdates() {
+        return this.equals(UPDATE_FOUND);
+    }
+}
