a11cb25a66ae0c95023a3f24704809789ca06431
Leymooo <vjatseslav.maspanov@gmail.com>
2021-03-28 00:09:08 +0200

    Fix support with new versions of ProtocolLib

--- src/main/java/ru/leymooo/fixer/utils/PlayerUtils.java
+++ src/main/java/ru/leymooo/fixer/utils/PlayerUtils.java
@@ -1,38 +1,45 @@
 package ru.leymooo.fixer.utils;
 
+import com.comphenix.net.sf.cglib.proxy.Factory;
+import com.comphenix.protocol.events.PacketEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import com.comphenix.net.sf.cglib.proxy.Factory;
-import com.comphenix.protocol.events.PacketEvent;
-
 
 public class PlayerUtils {
-    
+
     private static boolean isNewProtocolLib = false;
-    
+    private static boolean isNewestProtocolLib = true;
+
     static {
         try {
             Class.forName("com.comphenix.protocol.injector.server.TemporaryPlayer");
             isNewProtocolLib = true;
-        } catch (ClassNotFoundException e) {}
+        } catch (ClassNotFoundException e) {
+        }
+        try {
+            Class.forName("com.comphenix.net.sf.cglib.proxy.Factory");
+            isNewestProtocolLib = false;
+        } catch (ClassNotFoundException e) {
+
+        }
     }
-    
+
     public static Player getPlayerFromEvent(PacketEvent event) {
         Player eventPlayer = event.getPlayer();
-        
+
         if (eventPlayer == null || !eventPlayer.isOnline()) return null;
-        
+
         if (isNewProtocolLib && event.isPlayerTemporary()) return null;
-        
+
         String playerName = eventPlayer.getName();
-        
-        if (playerName == null ) return null;
-        
+
+        if (playerName == null) return null;
+
         Player bukkitPlayer = Bukkit.getPlayerExact(playerName);
-        
-        if (bukkitPlayer == null || !bukkitPlayer.isOnline() || bukkitPlayer instanceof Factory) return null;
-                
+
+        if (bukkitPlayer == null || !bukkitPlayer.isOnline() || (!isNewestProtocolLib && bukkitPlayer instanceof Factory)) return null;
+
         return bukkitPlayer;
     }
 }
